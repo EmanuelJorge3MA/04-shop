3f7dec59e32d98856cd18b3fe9d7000e6701d7bf
EmanuelJorge3MA <emanuel.jorge.130@gmail.com>
2023-03-29 12:02:04 +0100

    shop 1.0

---
+++ src/styles/dark.ts
@@ -0,0 +1,17 @@
+import { createTheme } from ".";
+
+export const dark = createTheme(
+  {
+    colors: {
+      white: '#FFF',
+
+      gray900: '#F4F6F8',
+      gray800: '#c4c4cc',
+      gray300: '#0A66C2',
+      gray100: '#202024',
+
+      green500: '#520818',
+      green300: '#7C0C24',
+    },
+  }
+)
